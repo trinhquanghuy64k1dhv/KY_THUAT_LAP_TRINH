4248c6193a1babb8f26eed17755e2edc3f9a5913
trinhquanghuy64k1dhv <huyhocmatlab@gmail.com>
2024-12-13 14:24:39 +0700

    Update BAI TH 2 CAU 11.py

--- BAI_TH2/BAI TH 2 CAU 11.py
+++ BAI_TH2/BAI TH 2 CAU 11.py
@@ -1,11 +1,12 @@
 print("Sinh vien : Trinh Quang Huy")
 print("MSSV : 235752021610016")
 print("#################")
-l=[1, "python",4,7]
-k=['cse', 2, 'guntur',8]
-m=[]
-m.append(l);
-m.append(k);
+# Viet chuong trinh ket noi ds vao tu dien
+l=[1, "python",4,7] # Tao ds dau tien
+k=['cse', 2, 'guntur',8] # Tao ds thu 2
+m=[] # Tao ds trong
+m.append(l); # Them ds l vao m
+m.append(k); # Them ds k vao m
 print (m)
-d={1:1,2:k, 'combine_list':m}
+d={1:1,2:k, 'combine_list':m} # Tao tu dien voi cac khoa va gia tri
 print (d)

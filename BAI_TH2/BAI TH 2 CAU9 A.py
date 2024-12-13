20a60a71721b4eea1b3efa9ae97746721b7a3691
trinhquanghuy64k1dhv <huyhocmatlab@gmail.com>
2024-12-13 14:05:23 +0700

    Update BAI TH 2 CAU9 A.py

--- BAI_TH2/BAI TH 2 CAU9 A.py
+++ BAI_TH2/BAI TH 2 CAU9 A.py
@@ -1,12 +1,13 @@
 print("Sinh vien : Trinh Quang Huy")
 print("MSSV : 235752021610016")
 print("#################")
+# Nhap mot chuoi 
 str = input ("Enter a String:")
-dict ={}
-for n in str:
-    keys = dict.keys()
+dict ={} # Khoi tao mot dictionary rong
+for n in str: # Lap qua tung ki tu trong chuoi
+    keys = dict.keys() # Kiem tra xem ki tu da ton tai trong dictionary chua
     if n in keys:
-        dict[n] += 1
+        dict[n] += 1 # Neu co, tang gia trin len 1
     else:
-        dict[n] = 1
+        dict[n] = 1 # Neu chua, gan gia tri bang 1
 print (dict)        

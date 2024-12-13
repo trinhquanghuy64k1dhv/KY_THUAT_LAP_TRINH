d0054c3506526b570990d489922530fd80e39166
trinhquanghuy64k1dhv <huyhocmatlab@gmail.com>
2024-12-13 12:01:50 +0700

    Update BAI TH 2 CAU7.py

--- BAI_TH2/BAI TH 2 CAU7.py
+++ BAI_TH2/BAI TH 2 CAU7.py
@@ -1,9 +1,10 @@
 print("Sinh vien : Trinh Quang Huy")
 print("MSSV : 235752021610016")
 print("#################")
+# Viet chuong trinh de tao ra 1 dictionary chua (i,i*i)
 n=int(input("Nhap so nguyen n:"))
 d=dict()
-for i in range(1,n+1):
+for i in range(1,n+1): #Lap qua so tu 1 den n 
     d[i]=i*i
 
 print (d)    

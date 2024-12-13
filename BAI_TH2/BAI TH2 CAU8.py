a456d0f4dd6f7683c7b87b01d5cb4bc45bce0d57
trinhquanghuy64k1dhv <huyhocmatlab@gmail.com>
2024-12-13 13:52:31 +0700

    Update BAI TH2 CAU8.py

--- BAI_TH2/BAI TH2 CAU8.py
+++ BAI_TH2/BAI TH2 CAU8.py
@@ -2,11 +2,11 @@ print("Sinh vien : Trinh Quang Huy")
 print("MSSV : 235752021610016")
 print("#################")
 a, b = 1, 2
-total = 0
-print(a,end=" ")
-while (a <=4000000-1):
-    if a % 2 == 0:
-        total += a
+total = 0 # Bien de luu tong cac so fibonacci chia het cho 2
+print(a,end=" ") # In so dau tien cua day Fibonacci(a=1),cach nhau bang dau cach
+while (a <=4000000-1): #Vong lap chay den khi a nho hon hoac bang 4000000
+    if a % 2 == 0: # Kiem tra neu a chia het cho 2
+        total += a # cong gia tri cua a vao total
     a, b = b, a+b
-    print(a,end=" ")
-print("\n sum of prine numbers term in fibonacci series: ",total)    
+    print(a,end=" ") # In ra tung dong va cach nhau mot khoang trang
+print("\n sum of prine numbers term in fibonacci series: ",total)  #Tong cac so hang trong day fibonacci  

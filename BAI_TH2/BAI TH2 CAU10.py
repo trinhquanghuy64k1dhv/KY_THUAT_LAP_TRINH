e983efbf08d41b47135c69c79b99febefb08cb43
trinhquanghuy64k1dhv <huyhocmatlab@gmail.com>
2024-12-13 14:17:12 +0700

    Update BAI TH2 CAU10.py

--- BAI_TH2/BAI TH2 CAU10.py
+++ BAI_TH2/BAI TH2 CAU10.py
@@ -2,7 +2,7 @@ print("Sinh vien : Trinh Quang Huy")
 print("MSSV : 235752021610016")
 print("#################")
 a="hi i am python programmer"
-b=a.split()
+b=a.split() # Tach chuoi thanh danh sach cac tu
 print (b)
-c=" ".join(b)
+c=" ".join(b) # Ghep ds lai thanh mot chuoi
 print (c)
